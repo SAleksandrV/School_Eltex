50731ffe76188bde3397a7fdf03607b45f39152e
Александр Смельчаков <58877255+SAleksandrV@users.noreply.github.com>
2023-06-20 15:35:56 +0700

    Add function find

--- task_04.c
+++ task_04.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define SIZE 3
+#define SIZE 100
 
 /* Программа телефонный справочник */
 struct phone_directory {
@@ -13,10 +13,10 @@ struct phone_directory {
 
 // Добавление абонента
 void add_subscriber(struct phone_directory *group, int *count);
-// Удаление абонента по фамилии
+// Удаление абонента по номеру телефона
 void delete_subscriber(struct phone_directory *group, int *count);
 // поиск абонента/абонентов по имени или фамилии или номеру телефона
-
+void find_subscriber(struct phone_directory *group, int *count);
 // Вывод телефонного справочника
 void output_telephone_directory(struct phone_directory *group, int *count);
 
@@ -29,19 +29,15 @@ int main(void) {
         scanf("%d", &number);
         switch (number) {
             case 1:
-                printf("Adding a subscriber to the phone directory\n");
                 add_subscriber(group, &count);
                 break;
             case 2:
-                printf("We remove the subscriber from the telephone directory\n");
                 delete_subscriber(group, &count);
                 break;
             case 3:
-                printf("Pass");
-                // find();
+                find_subscriber(group, &count);
                 break;
             case 4:
-                printf("Viewing the entire phone directory\n");
                 output_telephone_directory(group, &count);
                 break;
             case 5:
@@ -54,7 +50,6 @@ int main(void) {
     }
     return 0;
 }
-
 
 void add_subscriber(struct phone_directory *group, int *count) {
     if (*count == SIZE) {
@@ -101,6 +96,56 @@ void delete_subscriber(struct phone_directory *group, int *count) {
     }
 }
 
+void find_subscriber(struct phone_directory *group, int *count) {
+    char name[50], surname[100], phone_number[15];
+    int point = 1;
+     if (*count == 0) {
+        printf("\n\tThe phone directory is empty\n");
+    } else {
+        printf("\tChoose how to search for a subscriber:\n1 - by name\n2 - by surname\n3 - by phone number\n");
+        int temp;
+        scanf("%d", &temp);
+        if (temp == 1) {
+            printf("Enter the subscriber's name:\n");
+            scanf("%s", name);
+            for (int i = 0; i < *count; i++) {
+                if (strcmp(name, group[i].name) == 0) {
+                    printf("\n\tSubscriber\nNAME\t\t %s\nLAST NAME\t %s\nPHONE NUMBER\t %s\n", group[i].name, group[i].surname, group[i].phone_number);
+                    point--;
+                }
+            }
+            if (point > 0) {
+                printf("There is no such subscriber %s in the telephone directory\n", name);
+            }
+        } else if (temp == 2) {
+            printf("Enter the subscriber's last name:\n");
+            scanf("%s", surname);
+            for (int i = 0; i < *count; i++) {
+                if (strcmp(surname, group[i].surname) == 0) {
+                    printf("\n\tSubscriber\nNAME\t\t %s\nLAST NAME\t %s\nPHONE NUMBER\t %s\n", group[i].name, group[i].surname, group[i].phone_number);
+                    point--;
+                }
+            }
+            if (point > 0) {
+                printf("There is no such subscriber %s in the telephone directory\n", surname);
+            }
+        } else if (temp == 3) {
+            printf("Enter the subscriber's phone number:\n");
+            scanf("%s", phone_number);
+            for (int i = 0; i < *count; i++) {
+                if (strcmp(phone_number, group[i].phone_number) == 0) {
+                    printf("\n\tSubscriber\nNAME\t\t %s\nLAST NAME\t %s\nPHONE NUMBER\t %s\n", group[i].name, group[i].surname, group[i].phone_number);
+                    point--;
+                }
+            }
+            if (point > 0) {
+                printf("There is no such subscriber %s in the telephone directory\n", phone_number);
+            }
+        }
+    }
+}
+
+
 void output_telephone_directory(struct phone_directory *group, int *count) {
     if (*count == 0) {
         printf("\n\tThe phone directory is empty\n");
@@ -111,7 +156,4 @@ void output_telephone_directory(struct phone_directory *group, int *count) {
             printf("%d\t%-20s\t%-50s %-20s\n", i + 1, group[i].name, group[i].surname, group[i].phone_number);
         }
     }
-
 }
-
-
