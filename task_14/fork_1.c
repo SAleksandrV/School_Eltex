293fa32188b216c6d099b540ca49146a3cbe0818
Александр Смельчаков <58877255+SAleksandrV@users.noreply.github.com>
2023-07-11 21:47:11 +0700

    Add Makefile

--- task_14/fork_1.c
+++ task_14/fork_1.c
@@ -11,12 +11,12 @@ int main(void) {
     int status;
     child_pid = fork();
     if (child_pid == 0) {
-        printf("Child pid = %d\n", getpid());
+        printf("Child pid = %d. Parent pid = %d\n", getpid(), getppid());
         exit(5);
     } else {
-        printf("Parent pid = %d\n", getpid());
+        printf("Parent pid = %d. Parent pid = %d\n", getpid(), getppid());
         wait(&status);
-        printf("Status = %d\n", WEXITSTATUS(status));
+        printf("Status = %d\n\n", WEXITSTATUS(status));
     }
 
     // second variant program
@@ -26,12 +26,12 @@ int main(void) {
             perror("fork");
             return -1;
         case 0:
-            printf("Child my pid = %i, returned pid = %i\n", getpid(), new_pid);
+            printf("Child my pid = %i, returned pid = %i. Parent pid = %d\n", getpid(), new_pid, getppid());
             exit(7);
             break;
         default:
             wait(&status);
-            printf("Parent my pid = %i, returned pid = %i status = %d\n", getpid(), new_pid, WEXITSTATUS(status));
+            printf("Parent my pid = %i, returned pid = %i status = %d. Parent pid = %d\n", getpid(), new_pid, WEXITSTATUS(status), getppid());
             break;
     }
     return 0;
